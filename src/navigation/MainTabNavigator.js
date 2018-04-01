e348434bf4e5ee043c1f9a65e997afe5ccfa6fea
Martin Brännstrand <>
2018-04-01 13:00:03 +0200

    Navigation tabs + modal

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,44 +1,65 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { TabNavigator, TabBarBottom } from 'react-navigation';
+import React from "react"
+import { Platform, View } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
+import { TabNavigator, TabBarBottom } from "react-navigation"
 
-import Colors from '../constants/Colors';
+import Colors from "../constants/Colors"
 
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import HomeScreen from "../screens/HomeScreen"
+import SearchScreen from "../screens/SearchScreen"
+import AlertsScreen from "../screens/AlertsScreen"
+import ProfileScreen from "../screens/ProfileScreen"
 
 export default TabNavigator(
   {
     Home: {
       screen: HomeScreen,
     },
-    Links: {
-      screen: LinksScreen,
+    Search: {
+      screen: SearchScreen,
     },
-    Settings: {
-      screen: SettingsScreen,
+    LogPlay: {
+      screen: View,
+    },
+    Alerts: {
+      screen: AlertsScreen,
+    },
+    Profile: {
+      screen: ProfileScreen,
     },
   },
   {
     navigationOptions: ({ navigation }) => ({
+      tabBarOnPress: ({
+        scene: { route: { routeName }, index },
+        jumpToIndex,
+      }) => {
+        routeName === "LogPlay"
+          ? navigation.navigate("LogPlayModal")
+          : jumpToIndex(index)
+      },
       tabBarIcon: ({ focused }) => {
-        const { routeName } = navigation.state;
-        let iconName;
+        const { routeName } = navigation.state
+
+        let iconName
         switch (routeName) {
-          case 'Home':
+          case "Home":
             iconName =
-              Platform.OS === 'ios'
-                ? `ios-information-circle${focused ? '' : '-outline'}`
-                : 'md-information-circle';
-            break;
-          case 'Links':
-            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
-            break;
-          case 'Settings':
+              Platform.OS === "ios"
+                ? `ios-information-circle${focused ? "" : "-outline"}`
+                : "md-information-circle"
+            break
+          case "Links":
             iconName =
-              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+              Platform.OS === "ios"
+                ? `ios-link${focused ? "" : "-outline"}`
+                : "md-link"
+            break
+          case "Settings":
+            iconName =
+              Platform.OS === "ios"
+                ? `ios-options${focused ? "" : "-outline"}`
+                : "md-options"
         }
         return (
           <Ionicons
@@ -47,12 +68,12 @@ export default TabNavigator(
             style={{ marginBottom: -3, width: 25 }}
             color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
           />
-        );
+        )
       },
     }),
     tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
+    tabBarPosition: "bottom",
     animationEnabled: false,
     swipeEnabled: false,
-  }
-);
+  },
+)
